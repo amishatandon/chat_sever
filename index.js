f600af9f716f6361ed706ffda481349c738fc838
amishatandon <amishatandon2003@gmail.com>
2024-06-12 20:14:36 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+var http = require("http");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+var server = http.createServer(app);
+var io = require("socket.io")(server);
+app.use(express.json());
+
+io.on("connection", (socket)=>{
+    console.log("connected");
+    console.log(socket.id , "has joined");
+    socket.on("/test", (msg)=>{
+        console.log(msg);
+    })
+});
+server.listen(port,"0.0.0.0", ()=>{
+    console.log("server started", port);
+});
